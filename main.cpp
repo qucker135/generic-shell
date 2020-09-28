037a13a9335cfdee986597c5c70530028198b341
qucker <qucker135@gmail.com>
2020-09-28 18:26:24 +0200

    Limit # entries

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <ncurses.h>
 #include <unistd.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+int y=0,x=0;
+int sign = 0;
+string bfr = "";
+vector<string> hist;
+int hist_ptr=0;
+bool newline = true;
+int cur_line = 0;
+
 int main(){
 	initscr();
-	printw(">");
-	refresh();
-	sleep(1);
+	do
+	{
+		getmaxyx(stdscr,y,x);
+		move(cur_line,0);
+		printw(">");
+		refresh();
+		sign = getch();
+		
+
+
+		if(cur_line>=y-1) newline = false;
+		if(newline) cur_line++;
+	}while(sign!='q');
 	endwin();
 	return 0;
 }
