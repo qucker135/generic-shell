ac9be8d5b5fdf5530f573325dc834495da6ebdfd
qucker <qucker135@gmail.com>
2020-09-29 16:24:34 +0200

    Fix visual bugs

--- main.cpp
+++ main.cpp
@@ -11,8 +11,47 @@ string bfr = "";
 int bfr_pos=0;
 vector<string> hist;
 int hist_clear_ptr=0;
-bool newline = true;
+//bool newline = true;
 int cur_line = 0;
+
+void show_hist(){
+	getmaxyx(stdscr,y,x);
+	clear();
+	move(0,0);
+	cur_line = 0;
+	if(hist.size()-hist_clear_ptr<y){
+		for(int i=hist_clear_ptr;i<hist.size();i++){
+			printw(">");
+			for(int j=0;j<hist.at(i).length();j++){
+				printw("%c",hist.at(i)[j]);
+			}
+			cur_line++;
+			move(cur_line,0);
+			//printw("\n");
+		}
+	}
+	else{
+		for(int i=hist.size()-y+1;i<hist.size();i++){
+			printw(">");
+			for(int j=0;j<hist.at(i).length();j++){
+				printw("%c",hist.at(i)[j]);
+			}
+			cur_line++;
+			move(cur_line,0);
+			//printw("\n");
+		}
+	}
+	//cur_line operations
+	
+
+	//move(cur_line,0);
+	printw(">");
+	/*for(int i=1;i<x;i++){
+		printw(" ");
+	}
+	move(cur_line,1);*/
+	refresh();
+}
 
 int main(){
 	initscr();
@@ -22,10 +61,7 @@ int main(){
 	{
 		bfr = "";
 		bfr_pos=0;
-		getmaxyx(stdscr,y,x);
-		move(cur_line,0);
-		printw(">");
-		refresh();
+		show_hist();	
 		do{
 			sign = getch();
 
@@ -34,12 +70,17 @@ int main(){
 				bfr_pos++;
 			}
 
+			else if(sign==410){//resize window
+				cur_line = (hist.size()-hist_clear_ptr) * (hist.size()-hist_clear_ptr<y) + (y-1) * (hist.size()-hist_clear_ptr>=y);
+				show_hist();
+			}
+
 		}while(sign!=10 && sign!=4);//ENTER and ^D
 		if(sign==10){
 			hist.push_back(bfr);
-			if(cur_line>=y-1) newline = false;
+			/*if(cur_line>=y-1) newline = false;
 			else newline = true;
-			if(newline) cur_line++;
+			if(newline) cur_line++;*/
 		}
 
 	}while(bfr!="exit" && bfr!="quit" && sign!=4);//exit, quit and ^D
